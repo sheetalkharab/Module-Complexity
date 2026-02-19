c11a30651bef2d8b95e115a92b96586a4af36944
sheetalkharab <sheetalrana89@gmail.com>
2026-02-19 10:51:29 +0000

    Sprint 2: Improve code with precomputing - only 2 files

--- Sprint-2/improve_with_precomputing/common_prefix/common_prefix.py
+++ Sprint-2/improve_with_precomputing/common_prefix/common_prefix.py
@@ -7,12 +7,18 @@ def find_longest_common_prefix(strings: List[str]):
 
     In the event that an empty list, a list containing one string, or a list of strings with no common prefixes is passed, the empty string will be returned.
     """
+    if len(strings) < 2:
+        return ""
+    
+    # Precompute: Sort so similar strings are near each other
+    strings = sorted(strings)
+
     longest = ""
-    for string_index, string in enumerate(strings):
-        for other_string in strings[string_index+1:]:
-            common = find_common_prefix(string, other_string)
-            if len(common) > len(longest):
-                longest = common
+    
+    for i in range(len(strings) - 1) :
+        common = find_common_prefix(strings[i], strings[i+1])
+        if len(common) > len(longest):
+            longest = common
     return longest
 
 
--- Sprint-2/improve_with_precomputing/count_letters/count_letters.py
+++ Sprint-2/improve_with_precomputing/count_letters/count_letters.py
@@ -1,13 +1,33 @@
+from typing import Set
+
+
 def count_letters(s: str) -> int:
     """
     count_letters returns the number of letters which only occur in upper case in the passed string.
     """
-    only_upper = set()
+    # only_upper = set()
+    # for letter in s:
+    #     if is_upper_case(letter):
+    #         if letter.lower() not in s:
+    #             only_upper.add(letter)
+    # return len(only_upper)
+    uppercase_letters : Set[str] = set()
+    lowercase_letters : Set[str] = set()
+
+    # Precompute uppercase and lowercase letters
     for letter in s:
         if is_upper_case(letter):
-            if letter.lower() not in s:
-                only_upper.add(letter)
-    return len(only_upper)
+            uppercase_letters.add(letter)
+        elif letter.isalpha():
+            lowercase_letters.add(letter)  
+
+    # Count uppercase letters that don't appear in lowercase
+    count = 0 
+    for letter in uppercase_letters:
+        if letter.lower() not in lowercase_letters:    
+            count +=1
+            
+    return count         
 
 
 def is_upper_case(letter: str) -> bool:
